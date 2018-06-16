29d3607ae4e2c3dc5517ed3efff69d119b2d836c
Simon Stroh <simon_stroh@outlook.com>
2018-06-15 19:21:33 -0700

    Final changes before testing

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const request = require('request')
 const parser = require('body-parser')
 const axios = require('axios')
-
 const app = express()
+app.use(parser.json())
 app.use(express.static(__dirname))
 app.get('/products/:id/images', (req, res) => {
   request(`http://localhost:3004${req.url}`, (error, response, body) => {
@@ -12,32 +12,53 @@ app.get('/products/:id/images', (req, res) => {
   })
 })
 app.get('/productDetails/:id', (req, res) => {
-  request(`http://localhost:3001${req.url}`, (error, response, body) => {
-    if (error) throw new Error(error)
-    res.end(body)
-  })
-})
-app.get('/youMayAlsoLike/:id', (req, res) => {
-  console.log("Request from", req.url)
-  request(`http://localhost:3003${req.url}`, (error, response, body) => {
-    if (error) throw new Error(error)
-    res.end(body)
-  })
-})
-app.get('/reviews/:id', (req, res) => {
-  console.log("Request from", req.url)
-  axios.get(`http://localhost:3002${req.url}`)
+  console.log(`http://localhost:3001${req.url}`)
+  axios.get(`http://localhost:3001${req.url}`)
     .then((response) => {
-      res.send(response)
+      res.send(response.data);
     })
     .catch((err) => {
-      throw err
-    })
-})
-app.get('/reviews/public/:file', (req, res) => {
+      console.log('your axios has an error', err)
+    });
+});
+app.get('/reviews-module/reviews/:id', (req, res) => {
+  console.log(`http://localhost:3002${req.url}`);
   request(`http://localhost:3002${req.url}`, (error, response, body) => {
-    if (error) throw new Error(error)
-    res.end(body)
-  })
-})
+    if (error) throw new Error(error);
+    res.end(body);
+  });
+});
+
+app.put('/reviews-module/reviews', (req, res) => {
+  console.log('req.body', req.body);
+  axios.put(`http://localhost:3002${req.url}`, req.body)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch((err) => {
+      throw (err);
+    });
+});
+
+app.get('/youMayAlsoLike/:id', (req, res) => {
+  axios.get(`http://localhost:3003${req.url}`)
+    .then((response) => {
+      console.log('this is the response', response.data)
+      res.send(response.data);
+    })
+    .catch((err) => {
+      console.log('your axios has an error', err)
+    });
+});
+
+app.get('/products/:id/images', (req, res) => {
+  axios.get(`http://localhost:3004${req.url}`)
+    .then((response) => {
+      console.log('this is the response', response.data)
+      res.send(response.data);
+    })
+    .catch((err) => {
+      console.log('your axios has an error', err)
+    });
+});
 app.listen(3000)
